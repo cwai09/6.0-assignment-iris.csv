0aa0eb7f43bc17bfe3dfccfd13599407c3427757
ChueiWai Lai <111936307+cwai09@users.noreply.github.com>
2022-08-24 12:12:03 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,8 +1,7 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 
-st.header("App1: Non-Show Guest Today")
+st.header("App1: Non-Shown Guests Today")
 
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox('I agree the terms and conditions')
