9beb135ee2dc71e19d32af5430ce0030bd655f60
ChueiWai Lai <111936307+cwai09@users.noreply.github.com>
2022-08-24 12:09:14 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.header("App1: Attendence of Chefs")
+st.header("App1: Non-Show Guest Today")
 
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox('I agree the terms and conditions')
@@ -10,4 +10,3 @@ if show:
     'Intplan': ['yes', 'yes', 'yes', 'no'],
     'Churn Status': [0, 0, 0, 1]
 }))
-    
